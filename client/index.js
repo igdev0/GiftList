882a40692ed6a6e2efa886cad27244fecd06d06e
igdev <dorultanianos@gmail.com>
2025-04-01 11:34:10 +0300

    complete challenge

--- client/index.js
+++ client/index.js
@@ -5,10 +5,16 @@ const MerkleTree = require('../utils/MerkleTree');
 const serverUrl = 'http://localhost:1225';
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+  // TODO: how do we prove to the server we're on the nice list? ✅
+  const name = 'Chris Windler';
 
+  const tree = new MerkleTree(niceList);
+  const idx = niceList.indexOf(name);
+  const proof  = tree.getProof(idx);
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+    leaf: name,
+    proof: proof
+    // TODO: add request body parameters here! ✅
   });
 
   console.log({ gift });
--- utils/example.js
+++ utils/example.js
@@ -7,8 +7,7 @@ const merkleTree = new MerkleTree(niceList);
 
 // get the root
 const root = merkleTree.getRoot();
-
-// find the proof that norman block is in the list 
+// find the proof that norman block is in the list
 const name = 'Norman Block';
 const index = niceList.findIndex(n => n === name);
 const proof = merkleTree.getProof(index);
